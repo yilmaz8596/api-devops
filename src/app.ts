ac55156cb654f52bb94ec54740b7c576a5a2e9d8
Fırat Yılmaz <yilmaz8596@gmail.com>
2026-02-13 15:08:38 +0300

    initial commit

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+
+import express from 'express';
+import { Request,Response } from 'express';
+
+
+const app = express();
+
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello, World!');
+});
+
+
+export default app;
